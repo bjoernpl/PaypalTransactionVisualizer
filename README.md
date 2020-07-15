2ceed1ea1b5999a66a4e9d2cabdf3bf3b8080969
Björn Plüster <bjoern.pl@outlook.de>
2020-07-15 14:16:42 +0200

    Initial project commit. Moved from private repo and removed sensitive data.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# PaypalTransactionVisualizer
+# Paypal Transaction Visualizer
+This visualizer is a tool that can be used to track and analyze all your PayPal expenditures.
+
+This project was used as a way to try out many more advanced python features such as:
+- functools.reduce()
+- map()
+- dict and list comprehensions
+
+[BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/) library is used for  HTML Parsing, while all visualization
+is done using [Matplotlib](https://matplotlib.org/).
+
+## 1. Installation
+To use this project you can:
+
+1. Run it online using [DataLore](https://datalore.io/notebook/ZzZiQkJ5dGKsGciTsZaT0t/PsjMBL65cDv0ruSQKuPCGL/)
+    * Follow the link to open the notebook virtual environment
+    * Navigate to ``Tools -> Attached files`` and upload your ``payments.html``
+     (see section 2. for more info on how to access your your PayPal transaction data)
+    * Run all cells to see results
+2. Or you can run the notebook locally
+    * First clone the repository: ```git clone https://github.com/bjoernpl/PaypalTransactionVisualizer.git```
+    * Navigate to src directory: ```cd PaypalTransactionVisualizer/src```
+    * Launch the notebook with ```jupyter notebook TransactionVisualizer.ipynb```
+    * Exchange the html in ```payments.html``` with you PayPal transaction html
+    * Run all cells to see the results
+
+## 2. Retrieving Paypal transaction html
+1. Log in to your account at [paypal.com](paypal.com)
+2. Navigate to [paypal.com/myaccount/transactions](https://www.paypal.com/myaccount/transactions/)
+3. Choose the time frame you want to analyze
+4. Right-click the completed transactions section and select ``inspect``
+5. Right-click 
+    ````html
+    <ul class="transactionList js_transactionList">
+    ````
+6. Select ``Edit as html`` followed by ``ctrl+a ctrl+c``
+7. Replace all text in ``payments.html`` with the copied text
---
+++ src/transaction_utils.py
@@ -0,0 +1,66 @@
+"""MIT License
+
+Copyright (c) 2020 Björn Plüster
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+from typing import Tuple
+
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+from datetime import date, timedelta
+
+
+def clean_date(date_string: str) -> date:
+    """
+    'Cleans' the date found in PayPal html. Instead of using a date,
+    they use 'today' and 'yesterday' or 'heute' and 'gestern' in
+    german respectively.
+
+    :param date_string: The date string found in the html
+    :return: a date corresponding to the input
+    """
+    if date_string in ["gestern", "yesterday"]:
+        return date.today() - timedelta(days=1)
+    elif date_string in ["heute", "today"]:
+        return date.today()
+    else:
+        return parse(date_string).date()
+
+
+def get_transaction_data(transaction: BeautifulSoup) -> Tuple[str, float, date]:
+    """
+    Gets the valuable data from a single transaction.
+
+    :param transaction: the BeautifulSoup of a transaction found in
+    paypal transactions html
+    :return: a tuple of (name of other party, transaction amount, date)
+    """
+    name = transaction.find("strong", {"class": "counterparty-text"}).string
+
+    amount_soup = transaction.find("span", {"class": "netAmount"})
+    # html is different for receiving and sending transactions
+    if not amount_soup:
+        amount_soup = transaction.find_all("span", {"class": "isPositive"})[1]
+        amount = float(amount_soup.string.replace(",", "."))
+    else:
+        amount = -float(amount_soup.string.replace(",", "."))
+
+    date_string = transaction.find("span", {"class": "relative-time"}).next
+
+    return name.strip(), amount, clean_date(date_string)
